a8fbabef9ff556dbc8953a03f54f5b1fc474041f
Bryan Y <cs337bryan@gmail.com>
2016-01-25 18:19:09 -0800

    Auth now a core implementation of the system

--- app/Core/Config.example.php
+++ app/Core/Config.example.php
@@ -16,6 +16,8 @@ use Helpers\Session;
 
 /**
  * Configuration constants and options.
+ *
+ * RENAME THIS FILE TO Config.php
  */
 class Config
 {
@@ -65,12 +67,12 @@ class Config
         /**
          * Database name.
          */
-        define('DB_NAME', 'dbname');
+        define('DB_NAME', 'mydb');
 
         /**
          * Database username.
          */
-        define('DB_USER', 'root');
+        define('DB_USER', 'username');
 
         /**
          * Database password.
@@ -80,12 +82,12 @@ class Config
         /**
          * PREFER to be used in database calls default is smvc_
          */
-        define('PREFIX', 'smvc_');
+        define('PREFIX', 'uap_');
 
         /**
          * Set prefix for sessions.
          */
-        define('SESSION_PREFIX', 'smvc_');
+        define('SESSION_PREFIX', 'uap_');
 
         /**
          * Optional create a constant for the name of the site.
@@ -107,6 +109,75 @@ class Config
          * Set timezone.
          */
         date_default_timezone_set('Europe/London');
+
+        /**
+         * reCAPTCHA keys
+         */
+        define("RECAP_PUBLIC_KEY", ''); // reCAPCHA site key
+        define("RECAP_PRIVATE_KEY", ''); // reCAPCHA secret key
+
+
+
+        /**
+         * Auth Helpers necessities
+         */
+        // Name of website to appear in emails
+        define("SITE_NAME", "Test");
+
+        // Email FROM address for Auth emails (Activation, password reset...)
+        define("EMAIL_FROM", "email@example.com");
+
+        // INT : Max number of attempts for login before user is locked out
+        define("MAX_ATTEMPTS", 5);
+
+        // URL to Auth Class installation root WITH trailing slash
+        define("BASE_URL", "/");
+
+        // Account activation route
+        define("ACTIVATION_ROUTE", 'activate');
+
+        // Account password reset route
+        define("RESET_PASSWORD_ROUTE", 'resetpassword');
+
+        //How long a session lasts : Default = +1 month
+        define("SESSION_DURATION", "+1 month");
+
+        //Max attempts for logging in
+        define("SECURITY_DURATION", "+5 minutes");
+
+        //INT cost of BCRYPT algorithm
+        define("COST", 10);
+
+        //INT hash length of BCRYPT algorithm
+        define("HASH_LENGTH", 22);
+
+        // Language of Auth Class output : en / fr /es / de
+        define("LOC", "es");
+
+        // min length of username
+        define('MIN_USERNAME_LENGTH', 5);
+
+        // max length of username
+        define('MAX_USERNAME_LENGTH', 30);
+
+        // min length of password
+        define('MIN_PASSWORD_LENGTH', 5);
+
+        // max length of password
+        define('MAX_PASSWORD_LENGTH', 30);
+
+        //max length of email
+        define('MAX_EMAIL_LENGTH', 100);
+
+        // min length of email
+        define('MIN_EMAIL_LENGTH', 5);
+
+        //random key used for password reset or account activation
+        define('RANDOM_KEY_LENGTH', 15);
+
+        $waittime = preg_replace("/[^0-9]/", "", SECURITY_DURATION); //DO NOT MODIFY
+        // this is the same as SECURITY_DURATION but in number format
+        define('WAIT_TIME', $waittime); //DO NOT MODIFY
 
         /**
          * Start sessions.
