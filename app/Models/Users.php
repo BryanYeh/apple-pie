eabe2aa4a0f2377666d631b6621c233ff2afe237
DaVaR <davar@thedavar.net>
2016-02-10 20:55:16 -0600

    test
    test

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -18,7 +18,7 @@ class Users extends Model
     {
 		var_dump($userID);
         $data = array('userID' => $userID ,'lastAccess' => date('Y-m-d G:i:s'));
-        $this->db->insert(PREFIX."users_online",$data);
+        $this->db->insert(PREFIX."users_online ",$data);
     }
 
     /**
@@ -30,7 +30,7 @@ class Users extends Model
         $query = $this->db->select('SELECT * FROM '.PREFIX.'users_online WHERE userID = :userID ', array(':userID' => $userID));
         $count = count($query);
         if($count == 0){
-            self::add($userID);
+            $this->add($userID);
         }else{
             $data = array('lastAccess' => date('Y-m-d G:i:s'));
             $where = array('userID' => $userID);
